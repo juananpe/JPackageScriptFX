b7fa9f46ca82a547aafd90cd7137cead9ec52b92
Juanan Pereira <juanan@ikasten.io>
2022-05-01 19:36:30 +0200

    Generate .deb instead of .rpm

--- jpackagefx-main/build_app_linux.sh
+++ jpackagefx-main/build_app_linux.sh
@@ -13,7 +13,7 @@ JAVA_VERSION=17
 MAIN_JAR="main-ui-$PROJECT_VERSION.jar"
 
 # Set desired installer type: "app-image", "rpm" or "deb".
-INSTALLER_TYPE=rpm
+INSTALLER_TYPE=deb
 
 echo "java home: $JAVA_HOME"
 echo "project version: $PROJECT_VERSION"
